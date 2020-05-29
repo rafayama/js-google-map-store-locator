023dba97e8feed765512ccdcfde5e1d04d944169
rafayama <rafa.yamashiro@gmail.com>
2020-05-29 14:50:44 -0400

    styles and js

---
+++ challenge-port/assets/scripts/app.js
@@ -0,0 +1,90 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "Sass/Scss",
+				weight: 12.3
+			}, {
+				text: "css3",
+				weight: 8
+			}, {
+				text: "javascript",
+				weight: 14
+			}, {
+				text: "jquery",
+				weight: 3
+			}, {
+				text: "programming",
+				weight: 7
+			}, {
+				text: "Drupal",
+				weight: 10
+			}, {
+				text: "Wordpress",
+				weight: 9
+			}, {
+				text: "Rafael",
+				weight: 15
+			}, {
+				text: "Shopify",
+				weight: 7
+			}, {
+				text: "Mailchimp",
+				weight: 7
+			}, {
+				text: "UI/UX",
+				weight: 7
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#200a33", "padding" : "0 20px" });
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
